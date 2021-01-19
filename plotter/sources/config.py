60afafd572225d5b1d534222e977c09b0c591497
Etienne Dumur <etienne.dumur@gmail.com>
2021-01-19 10:48:42 +0100

    Update config.py

--- plotter/sources/config.py
+++ plotter/sources/config.py
@@ -7,9 +7,9 @@ config = {
 # The soft will not allowed to go above that path
 'root' : 'S:/132-PHELIQS/132.05-LATEQS/132.05.01-QuantumSilicon',
 # Default display path, should be at least the root
-'path' : 'S:/132-PHELIQS/132.05-LATEQS/132.05.01-QuantumSilicon/bluelagoon/2021/2021014_RNBN7_BA5_35_S8_XXL_AAEE2_W9\data',
+'path' : 'S:/132-PHELIQS/132.05-LATEQS/132.05.01-QuantumSilicon/',
 # Folder having these names will be colored, easier to browse
-'enhancedFolder' : ['bluelagoon','Cryoconcept', 'RF4K_stick', 'smurf', 'Tritonito', 'data', '2020'],
+'enhancedFolder' : ['bluelagoon','Cryoconcept', 'RF4K_stick', 'smurf', 'Tritonito', 'triton', 'data', '2021'],
 # Other files will not appear in the plotter
 'authorizedExtension' : ['db', 'csv', 's2p'], 
 # Will not be displayed, usefull for some windows file
