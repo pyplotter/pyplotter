0a28bfcfa80841b44bb7fd1c87195012ec65e061
Etienne Dumur <etienne.dumur@gmail.com>
2022-04-28 14:00:37 +0200

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyplotter',
-      version='0.2.0',
+      version='0.3.0',
       author='Étienne Dumur',
       author_email='etienne.dumur@cea.fr',
       maintainer='Étienne Dumur',
