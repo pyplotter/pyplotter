63b9c42cd058ec76dfebb08eebeafc05612f4831
Etienne Dumur <etienne.dumur@gmail.com>
2022-05-10 12:09:58 +0200

    Make the repo a working installable pip package

--- setup.py
+++ setup.py
@@ -1,41 +1,5 @@
 # This Python file uses the following encoding: utf-8
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='pyplotter',
-      version='0.3.0',
-      author='Étienne Dumur',
-      author_email='etienne.dumur@cea.fr',
-      maintainer='Étienne Dumur',
-      maintainer_email='etienne.dumur@cea.fr',
-      description='A data browser and vizualizer for QCoDes database, csv, s2p and BlueFors logging files.',
-      url='https://github.com/pyplotter/pyplotter',
-      long_description=open('README.md', encoding="utf8").read(),
-      classifiers=[
-        'Programming Language :: Python :: 3',
-        'Intended Audience :: Science/Research',
-        'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering'
-      ],
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-          'qdarkstyle',
-          'lmfit',
-          'multiprocess',
-          'numpy>=1.17.0',
-          'pandas>=1.0.0',
-          'platformdirs',
-          'pyopengl',
-          'pyqt5',
-          'pyqt5-sip',
-          'pyqtgraph>=0.12.3',
-          'qcodes>=0.26.0',
-          'scikit-rf',
-          'scipy',
-      ],
-      entry_points={
-          'console_scripts':[
-              'plotter = pyplotter.pyplotter:main',
-              'pyplotter = pyplotter.pyplotter:main',
-          ]
-       })
+if __name__ == '__main__':
+    setup()
