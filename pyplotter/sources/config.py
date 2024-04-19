9b96d084d9765d2e217b923aa69b0af932a9032f
xlelephant <xlelephant@gmail.com>
2024-04-19 09:44:47 +0800

    add "plotHideInteractionPanel" to auto hide the interaction panel

--- pyplotter/sources/config.py
+++ pyplotter/sources/config.py
@@ -301,6 +301,7 @@ configPackage = {
 'plot1dSymbol' : ['o', 's', 't', 'd', '+'],
 'plot1dAntialias' : False,
 'plot2dcm' : 'Viridis', # Default colormap
+'plotHideInteractionPanel': False,
 # List of derivative for 2d plot
 # Each new entry should also be coded in the comboBoxDerivativeActivated method
 # see plot_2d_app.py.
