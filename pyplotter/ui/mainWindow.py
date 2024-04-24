ee3c6fa3c6c32aa3a68a52ca8010b0996ab5fa71
xlelephant <xlelephant@gmail.com>
2024-04-24 09:50:00 +0800

    Change ui layout

--- pyplotter/ui/mainWindow.py
+++ pyplotter/ui/mainWindow.py
@@ -3,7 +3,8 @@ import numpy as np
 from PyQt5 import QtGui, QtCore, QtWidgets
 from typing import Optional, Tuple
 
-from .mainWindowui import Ui_MainWindow
+# from .mainWindowui import Ui_MainWindow
+from .mainWindowui_r2c2 import Ui_MainWindow
 from .plot.plot1d.widgetPlot1d import WidgetPlot1d
 from .plot.plot2d.widgetPlot2d import WidgetPlot2d
 from .hBoxLayoutLabelPath import HBoxLayoutLabelPath
@@ -46,6 +47,8 @@ class MainApp(QtWidgets.QMainWindow):
         # Can't promote layout on qtdesigner...
         self.hBoxLayoutPath = HBoxLayoutLabelPath()
         self.ui.verticalLayout_12.addLayout(self.hBoxLayoutPath)
+        self.ui.splitter_1.setStretchFactor(1,1)
+        self.ui.splitter_1.setStretchFactor(2,2)
 
 
         self.signalRemoveProgressBar.connect(self.ui.statusBarMain.removeProgressBar)
