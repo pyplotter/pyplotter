b48870516ffa3493dbb7c1cc63e05bc43d893e3c
Etienne Dumur <etienne.dumur@gmail.com>
2024-07-09 10:37:27 +0200

    remove tentative fix for macOS

--- pyplotter/ui/dialogs/dialogLiveplot.py
+++ pyplotter/ui/dialogs/dialogLiveplot.py
@@ -603,9 +603,7 @@ class DialogLiveplot(QtWidgets.QDialog, Ui_LivePlot):
         self.pushButtonLivePlot.setText('Select database...')
         self.loadDataset = load_by_run_spec
 
-        # set DontUseNativeDialog to True to avoid error on mac
         qfd = QtWidgets.QFileDialog(self)
-        qfd.setOption(QtWidgets.QFileDialog.DontUseNativeDialog)
         fname = qfd.getOpenFileName(
             self,
             "Open QCoDeS database",
