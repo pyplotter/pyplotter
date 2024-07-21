61fa79c713d043529a750d1a3129b0beb3fa1e45
Liang Xiang <xiangliang@zju.edu.cn>
2024-07-21 09:49:51 +0800

    solve double clicking on qcode data
    clean up tableWidgetParameter.py

--- pyplotter/ui/tableWidgetParameter.py
+++ pyplotter/ui/tableWidgetParameter.py
@@ -1,76 +1,23 @@
-from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
+from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 import numpy as np
 
 from ..sources.config import loadConfigCurrent
 config = loadConfigCurrent()
-from ..sources.workers import loadDataFromRun, loadLabradDataFromRun
+from ..sources.workers.loadDataFromRun import LoadDataFromRunThread
+from ..sources.workers.loadLabradDataFromRun import LoadDataFromRunThread as LoadLabradDataFromRunThread
+from ..sources.labradDatavault import LabradDataset, dep_label
 from ..sources.functions import (clearTableWidget,
                                  getCurveId,
                                  getPlotRef,
                                  getPlotTitle,
                                  getWindowTitle,
-                                 isQcodesData,
                                  isLabradFolder,
                                  getParallelDialogWidthHeight,
                                  findXYIndex, 
                                  shapeData2d, 
                                  make_grid
                                  )
-from qcodes import load_by_run_spec as qcodes_load_by_run_spec
-from ..sources.labradDatavault import LabradDataset, dep_label
-
-
-def LoadDataFromRunThread(
-    curveId,
-    databaseAbsPath,
-    dependentParamName,
-    plotRef,
-    plotTitle,
-    runId,
-    windowTitle,
-    cb,
-    progressBarId,
-):
-    if isLabradFolder(databaseAbsPath):
-        worker = loadLabradDataFromRun.LoadDataFromRunThread(
-            curveId,
-            databaseAbsPath,
-            dependentParamName,
-            plotRef,
-            plotTitle,
-            runId,
-            windowTitle,
-            cb,
-            progressBarId,
-        )
-    elif isQcodesData(os.path.split(databaseAbsPath)[-1]):
-        worker = loadDataFromRun.LoadDataFromRunThread(
-            curveId,
-            databaseAbsPath,
-            dependentParamName,
-            plotRef,
-            plotTitle,
-            runId,
-            windowTitle,
-            cb,
-            progressBarId,
-        )
-    return worker
-
-
-def load_by_run_spec(databaseAbsPath, runId):
-    if isLabradFolder(databaseAbsPath):
-        labrad_dataset = LabradDataset(databaseAbsPath, noisy=False)
-        labrad_dataset.loadDataset(runId)
-        return labrad_dataset 
-    # elif isMongoDBFolder(databaseAbsPath):
-    #     dataset = switch_mongoDB_path(databaseAbsPath)
-    #     dataset.mount(runId - 1)  # MongoDB starts from 0
-    #     fixPath(dataset, databaseAbsPath)
-    #     return dataset
-    elif isQcodesData(os.path.split(databaseAbsPath)[-1]):
-        return qcodes_load_by_run_spec
 
 
 # Get the folder path for pictures
@@ -311,7 +258,7 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
         cb.setEnabled(False)
         QtCore.QThread.msleep(100)
 
-        if dataType in ['qcodes', 'Labrad']:
+        if dataType == 'qcodes':
             worker = LoadDataFromRunThread(curveId,
                                            databaseAbsPath,
                                            dependentParamName,
@@ -333,6 +280,32 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
 
             # Execute the thread
             self.threadpool.start(worker)
+
+        # Labrad data loading will take a while when huge data/parameters are stored in HDF5 files
+        # We use threading just like what is for qcodes data to avoid frozen windows.
+        elif dataType == 'Labrad':
+            worker = LoadLabradDataFromRunThread(curveId,
+                                           databaseAbsPath,
+                                           dependentParamName,
+                                           plotRef,
+                                           plotTitle,
+                                           runId,
+                                           windowTitle,
+                                           cb,
+                                           progressBarId)
+            # Connect signals
+            # To update the status bar
+            worker.signal.sendStatusBarMessage.connect(self.signalSendStatusBarMessage)
+            # To update the progress bar
+            worker.signal.updateProgressBar.connect(self.signalUpdateProgressBar)
+            # If data download failed
+            worker.signal.loadedDataEmpty.connect(self.signalLoadedDataEmpty)
+            # When data download is done
+            worker.signal.loadedDataFull.connect(self.signalLoadedDataFull)
+
+            # Execute the thread
+            self.threadpool.start(worker)
+
         elif dataType=='csv':
 
             self.signalCSVLoadData.emit(curveId,
@@ -395,17 +368,13 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
     ############################################################################
 
 
-    def getPlotDataParameters(self, databaseAbsPath, runId) -> None:
+    def getPlotLabradDataParameters(self, databaseAbsPath, runId) -> None:
         """
-        Gather the information from the current dataset.
-
-        Each dependent parameters must be treated independently since they
-        can each have a different number of independent parameters.
+        Gather the information from the current Labrad dataset.
         """
 
-        # Get dataset params
+        # Get dataset params for plotting
         paramsDependents = [i for i in self.dataset.getPlotDependents()]
-        # paramsIndependent = [i for i in self.dataset.getIndependents()]
 
         xParamNames = []
         xParamLabels = []
@@ -428,7 +397,6 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
             for dep_idx, paramsDependent in enumerate(paramsDependents):
 
                 depends_on = [i for i in self.dataset.getIndependents()]
-
                 param_x = depends_on[0]
                 xParamNames.append(param_x.name + " (name)")
                 xParamLabels.append(param_x.name)
@@ -473,23 +441,27 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
                         )
                     )
                 # We get the dialog position and size to tile the screen
-                if zParamNames[-1] == '':
+                if zParamNames[-1] == "":
                     dialogTilings = getParallelDialogWidthHeight(nbDialog=1, plotId=0)
-                    dialogX, dialogY, dialogWidth, dialogHeight = [tile[0] for tile in dialogTilings]
+                    dialogX, dialogY, dialogWidth, dialogHeight = [
+                        tile[0] for tile in dialogTilings
+                    ]
                 else:
                     # tile 3D plot with plot windows
                     num_2d_plots = len(paramsDependents)
                     dialogTilings = getParallelDialogWidthHeight(num_2d_plots, plotId=0)
-                    dialogX, dialogY, dialogWidth, dialogHeight = [tile[dep_idx] for tile in dialogTilings]
+                    dialogX, dialogY, dialogWidth, dialogHeight = [
+                        tile[dep_idx] for tile in dialogTilings
+                    ]
                 dialogXs.append(dialogX)
                 dialogYs.append(dialogY)
                 dialogWidths.append(dialogWidth)
                 dialogHeights.append(dialogHeight)
-                
+
                 d = self.dataset.getPlotData()
                 # Create empty data for the plot window launching
-                if zParamNames[-1]=='':
-                    data = (d[:,0], d[:,1 + dep_idx])
+                if zParamNames[-1] == "":
+                    data = (d[:, 0], d[:, 1 + dep_idx])
                 else:
 
                     # Find the effective x and y axis, see findXYIndex
@@ -502,13 +474,10 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
                         data = shapeData2d(
                             d[:, xi], d[:, yi], d[:, 2 + dep_idx], self.signalSendStatusBarMessage
                         )
-                    # data = (d[:,0],
-                    #         d[:,1],
-                    #         d[:,2 + dep_idx])
                 dataList.append(data)
-                curveId = getCurveId(databaseAbsPath=databaseAbsPath,
-                                    name=yParamNames[-1],
-                                    runId=runId)
+                curveId = getCurveId(
+                    databaseAbsPath=databaseAbsPath, name=yParamNames[-1], runId=runId
+                )
                 curveIds.append(curveId)
 
         return (
@@ -546,7 +515,6 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
         self.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Fixed)
         self.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Fixed)
 
-
         plotTitle       = getPlotTitle(databaseAbsPath=databaseAbsPath,
                                        runId=runId,
                                        experimentName=experimentName)
@@ -577,7 +545,6 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
                                    dataType,
                                    rowPosition)
 
-
         self.setSortingEnabled(True)
         self.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
         self.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
@@ -592,17 +559,17 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
         self.signalUpdateLabelCurrentSnapshot.emit(str(runId))
         self.signalUpdateLabelCurrentRun.emit(str(runId))
 
-
         ## Update label
         self.signalSendStatusBarMessage.emit('Ready', 'green')
 
-        # If a double click is detected, we launch a plot of the first parameter
-        if doubleClick:
+        # If a double click is detected, we launch a default plot of the Labrad Data
+        if doubleClick and isLabradFolder(databaseAbsPath):
             # self.parameterCellClicked(0, self._columnIndexes['plotted'])
-            self.dataset =load_by_run_spec(databaseAbsPath, runId)
+            self.dataset = LabradDataset(databaseAbsPath)
+            self.dataset.loadDataset(runId)
             plotTitle   = getPlotTitle(databaseAbsPath, runId, self.dataset.name)
             windowTitle = getWindowTitle(databaseAbsPath, runId, self.dataset.name)
-            plotParameters = self.getPlotDataParameters(databaseAbsPath, runId)
+            plotParameters = self.getPlotLabradDataParameters(databaseAbsPath, runId)
 
             for dep_idx, (xParamName, xParamLabel, xParamUnit,
                 yParamName, yParamLabel, yParamUnit,
@@ -615,9 +582,6 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
                     empty_data = (np.array([0., 1.]), 
                                   np.array([0., 1.]),
                                   np.array([[0., 1.], [0., 1.]]))
-                # curveId = getCurveId(databaseAbsPath=databaseAbsPath,
-                #     name=yParamName,
-                #     runId=runId)
                 if not self.cellWidget(dep_idx, self._columnIndexes['plotted']).isChecked():
                     self.signalAddPlot.emit(runId, # runId
                                             curveId, # curveId
@@ -666,9 +630,6 @@ class TableWidgetParameter(QtWidgets.QTableWidget):
                                              data[0],
                                              data[1],
                                              data[2])
-
-
-
 
     @QtCore.pyqtSlot(int, dict, str, str, str, str, str, str, str, str, int, bool)
     def slotAddRow(self, runId: int,
